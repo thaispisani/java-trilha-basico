05f3de8823e81ea45deeb71d98d65afc4825e602
Thais Santos <thaispisani_@hotmail.com>
2023-07-20 18:13:02 -0300

    implementação desafio conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da sua conta");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Informe sua agencia");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia
+                + ", conta: " + numeroConta + " e seu saldo: R$" + saldo + " já está disponível para saque.");
     }
 }
